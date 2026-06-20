67010667fc3c740a989e03857c595ba0040dae17
Dev Singh <dev.singh@joshtechnologygroup.com>
2026-06-20 12:39:43 +0530

    [DS_A1_01/navbar]: completed navbar section

--- src/script/index.js
+++ src/script/index.js
@@ -1,1 +1,15 @@
 import '../styles/main.scss';
+let popupOpen = false;
+
+let hamburger = document.querySelector('.navbar__hamburger');
+let popup = document.querySelector('.navbar__popup');
+
+hamburger.addEventListener('click', () => {
+    if (popupOpen === false) {
+        popup.style.display = 'flex';
+        popupOpen = true;
+    } else {
+        popup.style.display = 'none';
+        popupOpen = false;
+    }
+});
