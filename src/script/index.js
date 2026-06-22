ce8ee14aec793ecb742eb2e50b5fb7699e4f936e
Dev Singh <dev.singh@joshtechnologygroup.com>
2026-06-22 18:01:37 +0530

    [DS_A1_02/hero-section] : implemented hero image layer1 and 2 and hero-text section

--- src/script/index.js
+++ src/script/index.js
@@ -1,4 +1,5 @@
 import '../styles/main.scss';
+// import interFont from 'https://cdn.jsdelivr.net/npm/inter-font@3.19.0/+esm';
 let popupOpen = false;
 
 let hamburger = document.querySelector('#navbar-hamburger');
