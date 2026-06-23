5c963c4114470d54427caeeb44ad3e74ad579a4f
Dev Singh <dev.singh@joshtechnologygroup.com>
2026-06-23 12:47:35 +0530

    [DS_A1_01/navbar] : review fixes

--- src/script/index.js
+++ src/script/index.js
@@ -1,19 +1,15 @@
 import '../styles/main.scss';
 let popupOpen = false;
 
-let hamburger = document.querySelector('.navbar__hamburger');
-let popup = document.querySelector('.navbar__popup');
+let hamburger = document.querySelector('#navbar__hamburger');
+let popup = document.querySelector('#navbar__popup');
 
 hamburger.addEventListener('click', () => {
     if (popupOpen === false) {
-        popup.style.display = 'flex';
+        popup.classList.add('active');
         popupOpen = true;
-        hamburger.style.transform = 'rotate(90deg)';
-        hamburger.style.transition = 'transform 0.3s ease';
     } else {
-        popup.style.display = 'none';
+        popup.classList.remove('active');
         popupOpen = false;
-        hamburger.style.transform = 'rotate(0deg)';
-        hamburger.style.transition = 'transform 0.3s ease';
     }
 });
