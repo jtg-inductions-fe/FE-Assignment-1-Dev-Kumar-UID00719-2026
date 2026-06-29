c19065069d61185957db1bfdbf333beec8d81653
Dev Singh <dev.singh@joshtechnologygroup.com>
2026-06-29 10:38:42 +0530

    [DS_A1_03/travel-section] : review fixes

--- src/script/index.js
+++ src/script/index.js
@@ -1,14 +1,8 @@
 import '../styles/main.scss';
-// import interFont from 'https://cdn.jsdelivr.net/npm/inter-font@3.19.0/+esm';
 let popupOpen = false;
 
 let hamburger = document.querySelector('#navbar-hamburger');
 let popup = document.querySelector('#navbar-popup');
-
-// const links = document.querySelectorAll(".navbar__links");
-// links.forEach((link).addEventListener("click", (e)=>{
-//     e.preventDefault();
-// }))
 
 hamburger.addEventListener('click', () => {
     if (popupOpen === false) {
