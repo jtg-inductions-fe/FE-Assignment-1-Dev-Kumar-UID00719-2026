5774ddcd059f97d9c22eb1d5a67d14341aadadbb
Dev Singh <dev.singh@joshtechnologygroup.com>
2026-07-15 03:31:53 +0530

    [DS_A1_07/improvements] : implemented ** improvements pointed out in lead demo

--- src/script/navbar.js
+++ src/script/navbar.js
@@ -1,22 +1,23 @@
-import { BREAKPOINTS } from './constants';
-
 let popupOpen = false;
 
 const hamburger = document.querySelector('#navbar-hamburger');
 const popup = document.querySelector('#navbar-popup');
+const body = document.body;
 
 hamburger.addEventListener('click', () => {
     if (popupOpen === false) {
         popup.classList.add('navbar__popup--active');
         popupOpen = true;
+        body.classList.add('no-scroll');
     } else {
         popup.classList.remove('navbar__popup--active');
         popupOpen = false;
+        body.classList.remove('no-scroll');
     }
 });
 
 window.addEventListener('resize', () => {
-    if (window.innerWidth > BREAKPOINTS.DESKTOP) {
-        popup.classList.remove('navbar__popup--active');
-    }
+    popup.classList.remove('navbar__popup--active');
+    popupOpen = false;
+    body.classList.remove('no-scroll');
 });
